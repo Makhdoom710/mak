a839044b4b5379f560a49eeb2b5c242411434c94
Makhdoom710 <154961328+Makhdoom710@users.noreply.github.com>
2025-12-12 23:27:42 +0500

    Initialize add-sol

---
+++ src/components/YoutubeEmbed.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './YoutubeEmbed.module.scss'
+
+interface YoutubeEmbedProps {
+  url: string
+}
+
+const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ url }) => {
+  return (
+    <div className={styles.wrapper}>
+      <iframe src={url} allowFullScreen></iframe>
+    </div>
+  )
+}
+
+export default YoutubeEmbed
